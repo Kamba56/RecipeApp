2f6e4ee58b0b9f77842a734e35c4947a61351917
Kamba56 <tomajohn316@yahoo.com>
2022-12-22 19:39:09 +0100

    Add views for recipe and public recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,9 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = current_user.recipes
+  end
+
+  def public_recipe
+    @recipes = Recipe.where(public: true).order(created_at: :desc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :recipes
+  resources :recipes, only: [:index]
   devise_for :users
   resources :food, only: %i[index show new create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'public_recipe', to: 'recipes#public_recipe', as: :public_recipe
 
   # Defines the root path route ("/")
   root to: "food#index"
