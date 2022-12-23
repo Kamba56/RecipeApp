b098de5778522724a5df91dfb32c37a7aafd7de7
Kamba56 <tomajohn316@yahoo.com>
2022-12-23 13:01:16 +0100

    Add shopping list controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,6 +8,10 @@ class RecipesController < ApplicationController
     end
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
   def public_recipe
     @recipes = Recipe.where(public: true).order(created_at: :desc)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :recipes, only: [:index]
+  resources :recipes, only: [:index, :show]
   devise_for :users
   resources :food, only: %i[index show new create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'public_recipe', to: 'recipes#public_recipe', as: :public_recipe
+  get 'shopping_list', to: 'users#shopping_list', as: :shopping_list
 
   # Defines the root path route ("/")
   root to: "food#index"
