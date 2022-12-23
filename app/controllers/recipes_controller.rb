4bd29bb5a89bfd31664f603ce7dc209ddf8ea577
Kamba56 <tomajohn316@yahoo.com>
2022-12-23 15:37:31 +0100

    Add show view for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show, :public_recipe]
+
   def index
     unless current_user
       redirect_to public_recipe_path
@@ -10,9 +12,21 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+    @foods = find_foods(@recipe)
   end
 
   def public_recipe
     @recipes = Recipe.where(public: true).order(created_at: :desc)
+  end
+
+  private
+
+  def find_foods(recipe)
+    foods = []
+    recipe.recipe_foods.each do |x|
+      foods << Food.find(x.food_id)
+    end
+
+    return foods
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_20_193711) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_23_140725) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_193711) do
     t.string "name"
     t.string "measurement_unit"
     t.float "price"
-    t.integer "quantity"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -26,11 +25,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_193711) do
   end
 
   create_table "recipe_foods", force: :cascade do |t|
-    t.text "quantity"
     t.bigint "recipe_id", null: false
     t.bigint "food_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "quantity"
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
     t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
